085624ab6180fd61a4deec593da7d0f5ef38c9a4
Rosemary T <dev7673@proton.me>
2025-07-30 11:16:31 -0500

    Fix user grid creation

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const makeGrid = (gridContainer, gridSize) => {
-  if (gridSize > 99) return alert(`max grid size is 99, you have entered a grid size of ${gridSize}, please chose a smaller size`);
+
+  if (gridSize < 1) {
+    alert(
+      `minimum grid size is 1\nyou have entered a grid size of ${gridSize}\nplease chose a larger size`
+    );
+    return makeUserGrid();
+  }
+  if (gridSize > 99) {
+    alert(
+      `maximum grid size is 99\nyou have entered a grid size of ${gridSize}\nplease chose a smaller size`
+    );
+    return makeUserGrid();
+  }
+
+  gridContainer.textContent = '';
+
   for (let row = 0; row < gridSize; row++) {
+
     const gridRow = document.createElement('div');
     gridRow.className = 'grid-row';
+
     for (let col = 0; col < gridSize; col++) {
+
       const gridItem = document.createElement('div');
+
       gridItem.className = 'grid-item';
       gridRow.appendChild(gridItem);
+
     }
+
     gridContainer.appendChild(gridRow)
+
   }
 }
 
@@ -20,6 +42,7 @@ const paintItem = (query) => {
 
 const makeUserGrid = () => {
   let gridSize = prompt('enter size for grid (max: 99, default: 16)', 16);
+  if (gridSize === null) return;
   makeGrid(document.querySelector('.grid-container'), gridSize);
 }
 
